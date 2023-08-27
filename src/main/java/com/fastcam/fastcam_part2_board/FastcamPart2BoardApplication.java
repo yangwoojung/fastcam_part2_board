ee50dd38eae46a7f2b8813a206f3b7531130ee0f
woojung <woojung4554@gmail.com>
2023-08-27 15:00:11 +0900

    #6 - 부트 프로젝트 생성 (기존 깃 레파지토리의 document 와 같은 위치에 생성)

---
+++ src/main/java/com/fastcam/fastcam_part2_board/FastcamPart2BoardApplication.java
@@ -0,0 +1,13 @@
+package com.fastcam.fastcam_part2_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcamPart2BoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastcamPart2BoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fastcam/fastcam_part2_board/FastcamPart2BoardApplicationTests.java
@@ -0,0 +1,14 @@
+package com.fastcam.fastcam_part2_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastcamPart2BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("우정 test");
+	}
+
+}
